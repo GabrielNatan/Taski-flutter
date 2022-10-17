54df9455ea3a0ee85a3f707b52765560c3127f2c
gabriel natan <g.souza@grouplinknetwork.com>
2022-10-16 21:03:46 -0300

    create Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:page_transition/page_transition.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:task/home.dart';
+
 void main() {
   runApp(const TaskiApp());
 }
 
-
-class TaskiApp extends StatelessWidget {
+class TaskiApp extends StatefulWidget {
   const TaskiApp({super.key});
 
+  @override
+  State<TaskiApp> createState() => _TaskiAppState();
+}
+
+class _TaskiAppState extends State<TaskiApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,8 +22,35 @@ class TaskiApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: SplashScreen(),
     );
   }
 }
 
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splash: Column(children: [
+          Image.asset(
+            'assets/dart.png',
+            height: 90,
+            color: Colors.white,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ]),
+        nextScreen: Home(),
+        splashIconSize: 150,
+        duration: 3000,
+        splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.rightToLeft,
+        animationDuration: Duration(seconds: 1),
+        backgroundColor: Color.fromARGB(255, 62, 55, 255),
+        );
+        
+  }
+}
